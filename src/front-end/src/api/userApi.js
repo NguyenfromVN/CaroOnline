1ed54fa44a5222d761cafb8f5e4aa1de0ad901d0
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-12-19 22:59:25 +0700

    Added api for register feature and validate register's mail

--- src/front-end/src/api/userApi.js
+++ src/front-end/src/api/userApi.js
@@ -11,40 +11,54 @@ const userApi = {
                 // console.log(response);
             })
     },
-    getAllBoards: async ()=>{
+    register: async (email, username, password) => {
+        const url = '/register';
+        const response = await axiosClient.post(url, {
+            email: email,
+            username: username,
+            password: password
+        });
+        return response;
+    },
+    validate: async (username) => {
+        const url = `/validate/${username}`;
+        const response = await axiosClient.post(url);
+        return response;
+    },
+    getAllBoards: async () => {
         const url = '/board/get_all_boards';
         const response = await axiosClient.get(url);
         return response;
     },
-    getBoard: async function(boardId){
-        let boards=await this.getAllBoards();
-        for (let i=0; i<boards.length; i++){
-            if (boards[i].boardId==boardId)
+    getBoard: async function (boardId) {
+        let boards = await this.getAllBoards();
+        for (let i = 0; i < boards.length; i++) {
+            if (boards[i].boardId == boardId)
                 return boards[i];
         }
     },
-    takeTurn: async function(boardId, row, col){
+    takeTurn: async function (boardId, row, col) {
         const url = `/board/make_turn?boardId=${boardId}&row=${row}&col=${col}`;
         const response = await axiosClient.get(url);
         return response;
     },
-    joinBoard: async function(boardId){
-        const url = '/board/join_board?boardId='+boardId;
+    joinBoard: async function (boardId) {
+        const url = '/board/join_board?boardId=' + boardId;
         const response = await axiosClient.get(url);
         return response;
     },
-    createBoard: async function(boardId,name){
-        const url = '/board/create_board?boardId='+boardId+'&name='+name;
+    createBoard: async function (boardId, name) {
+        const url = '/board/create_board?boardId=' + boardId + '&name=' + name;
         const response = await axiosClient.get(url);
         return response;
     },
-    getBoardChat: async function(boardId){
-        const url = '/board/get_board_chat?boardId='+boardId;
+    getBoardChat: async function (boardId) {
+        const url = '/board/get_board_chat?boardId=' + boardId;
         const response = await axiosClient.get(url);
         return response;
     },
-    makeMessage: async function(boardId, time, content){
-        const url = '/board/make_message?boardId='+boardId+'&time='+time+'&content='+content;
+    makeMessage: async function (boardId, time, content) {
+        const url = '/board/make_message?boardId=' + boardId + '&time=' + time + '&content=' + content;
         const response = await axiosClient.get(url);
         return response;
     }
