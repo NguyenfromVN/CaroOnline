04c979c1dd14b5b64f3d9db853ae075d40a03990
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2020-12-27 21:26:05 +0700

    Changed 3x3 board size to 20x20

--- src/front-end/src/components/Board/index.js
+++ src/front-end/src/components/Board/index.js
@@ -10,7 +10,7 @@ import ws from '../../webSocketClient';
 const useStyles = makeStyles((theme) => ({
 }));
 
-const boardSize = 3;
+const boardSize = 20;
 
 const renderSquare = (props) => {
     // const winLine = props.winLine;
@@ -18,7 +18,7 @@ const renderSquare = (props) => {
         <Square
             value={props.value}
             onClick={props.onClick}
-            // highlight={(winLine && winLine.includes(i))}
+        // highlight={(winLine && winLine.includes(i))}
         />
     );
 }
@@ -40,38 +40,38 @@ export default function Board(props) {
             }
             setBoard(board);
             // get chat
-            let chat=await api.getBoardChat(boardId);
+            let chat = await api.getBoardChat(boardId);
             setChat(chat);
             // init web socket client
-            ws.createConnection(localStorage.getItem('username'),(topicName)=>{
-                let callbacks={
-                    chat: async function(){
-                        let newChat=await api.getBoardChat(boardId);
+            ws.createConnection(localStorage.getItem('username'), (topicName) => {
+                let callbacks = {
+                    chat: async function () {
+                        let newChat = await api.getBoardChat(boardId);
                         setChat(newChat);
                     },
-                    board: async function(){
+                    board: async function () {
                         let board = await api.getBoard(boardId);
                         setBoard(board);
                     },
-                    general: function(){
+                    general: function () {
                         // TODO
                     }
                 };
-                let arr=topicName.split('>>>')[0];
-                arr=arr.split('-');
-                let topic=arr[arr.length-1];
+                let arr = topicName.split('>>>')[0];
+                arr = arr.split('-');
+                let topic = arr[arr.length - 1];
                 if (callbacks[topic]) {
                     callbacks[topic]();
                 }
             });
-            let topic=`${board.userId1}-${board.userId2}`;
+            let topic = `${board.userId1}-${board.userId2}`;
             ws.subscribeTopic(`${topic}-board`);
             ws.subscribeTopic(`${topic}-chat`);
         })();
     }, []);
 
-    async function takeTurn(row, col){
-        await api.takeTurn(boardId,row,col);
+    async function takeTurn(row, col) {
+        await api.takeTurn(boardId, row, col);
         ws.notifyChange(`${board.userId1}-${board.userId2}-board`);
         let newBoard = await api.getBoard(boardId);
         setBoard(newBoard);
@@ -80,14 +80,14 @@ export default function Board(props) {
     function renderSquares() {
         if (!board.boardId)
             return;
-        let squares=board.history[board.history.length - 1].squares;
-        let jsx=[];
-        for (let i=0; i<boardSize; i++) {
-            let row=[];
-            for (let j=0; j<boardSize; j++) {
+        let squares = board.history[board.history.length - 1].squares;
+        let jsx = [];
+        for (let i = 0; i < boardSize; i++) {
+            let row = [];
+            for (let j = 0; j < boardSize; j++) {
                 row.push(renderSquare({
-                    value: squares[i*boardSize+j],
-                    onClick: ()=>takeTurn(i,j)
+                    value: squares[i * boardSize + j],
+                    onClick: () => takeTurn(i, j)
                 }));
             }
             jsx.push(<div key={i} className="board-row">{row}</div>);
@@ -96,10 +96,10 @@ export default function Board(props) {
     }
 
     // scroll to the bottom of chat frame
-    setTimeout(()=>{
+    setTimeout(() => {
         let element = document.getElementById("listChat");
         element.scrollTop = element.scrollHeight;
-    },0);
+    }, 0);
 
     return (
         <div>
@@ -110,7 +110,7 @@ export default function Board(props) {
                 <div >
                     {renderSquares()}
                 </div>
-                <Chat boardId={boardId} chat={chat} topicName={`${board.userId1}-${board.userId2}-chat`}/>
+                <Chat boardId={boardId} chat={chat} topicName={`${board.userId1}-${board.userId2}-chat`} />
             </div>
         </div>
     );
