53ae401bf785e7f202c84a8f315dd55f7dc74937
thinhphat755 <48512210+thinhphat755@users.noreply.github.com>
2021-01-04 22:17:45 +0700

    Updated some changes

--- src/front-end/src/components/Board/gameInfo.js
+++ src/front-end/src/components/Board/gameInfo.js
@@ -5,9 +5,11 @@ const GameInfo = (props) => {
     let winnerText = (props.board.winner) ? props.board.winner : "None";
     let nextTurnText = (props.board.nextTurn == username) ? "You" : "Opponent";
     let nextTurnChar = (username == props.board.userId1) ? "X" : "O";
+    let isPlayer = (username == props.board.userId1 || username == props.board.userId2);
+    let infoClassName = (isPlayer) ? "game-info-frame" : "game-info-frame-display-none"
 
     return (
-        <div className='game-info-frame'>
+        <div className={infoClassName}>
             <h1>Winner: {winnerText}</h1>
             <h1>Next turn: {nextTurnText}</h1>
             <h1>{nextTurnChar}</h1>
